c9aa25bb0d7e36032a8f432a711076aceb611a10
Alexey <thelegendofflaity@gmail.com>
2023-02-14 19:35:38 +0300

    added primitive Yaml Reader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import defaultClasses.Color;
 import defaultClasses.Coordinates;
 import defaultClasses.Person;
+import yamlsTools.YamlReader;
 import yamlsTools.YamlWriter;
 
 
@@ -9,13 +11,18 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Person person = new Person("John", new Coordinates(1, 2.0f), 3L, LocalDateTime.now(), "123456", null, null);
-        Person person2 = new Person("John", new Coordinates(1, 2.0f), 3L, LocalDateTime.now(), "123456", null, null);
-        System.out.println(person.getId());
-        String path = System.getenv("Lab5");
-        System.out.println(path);
-        System.out.println(person.toString());
-        YamlWriter yamlWriter = new YamlWriter();
-        yamlWriter.writeYaml(person, "src/main/resources/Person.yaml");
+        Person person = new Person("Alexey", new Coordinates(1, 3.0f),
+                2L, LocalDateTime.now(), "78910", null, null);
+        Person person2 = new Person("Dmitry", new Coordinates(2, 2.0f),
+                3L, LocalDateTime.now(), "123456", Color.BROWN, null);
+//        System.out.println(person.getId());
+//        String path = System.getenv("Lab5");
+//        System.out.println(path);
+//        System.out.println(person.toString());
+//        YamlWriter yamlWriter = new YamlWriter();
+//        yamlWriter.writeYaml(person, "src/main/resources/Person.yaml");
+//        yamlWriter.writeYaml(person2, "src/main/resources/Person.yaml");
+        YamlReader yamlReader = new YamlReader();
+        System.out.println(yamlReader.readYaml("Person.yaml", Person.class));
     }
 }
--- src/main/java/yamlsTools/YamlReader.java
+++ src/main/java/yamlsTools/YamlReader.java
@@ -1,5 +1,24 @@
 package yamlsTools;
 
-public class YamlReader {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class YamlReader {
+    public <T> T readYaml(String path, Class<T> type) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource(path)).getFile());
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.findAndRegisterModules();
+        T object_collection = null;
+        try {
+            object_collection = mapper.readValue(file, type);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return object_collection;
+    }
 }
