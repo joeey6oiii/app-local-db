c79b020ead8d901a49f78b13b49f008a73578e44
Alexey <thelegendofflaity@gmail.com>
2023-02-15 22:52:49 +0300

    did some changes to YamlReader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,21 @@
-import defaultClasses.Color;
-import defaultClasses.Coordinates;
-import defaultClasses.Person;
-import yamlsTools.YamlReader;
-import yamlsTools.YamlWriter;
+import defaultClasses.*;
+import yamlsTools.*;
 
 
-import java.io.IOException;
-import java.time.LocalDateTime;
+import java.io.*;
+import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Person person = new Person("Alexey", new Coordinates(1, 3.0f),
-                2L, LocalDateTime.now(), "78910", null, null);
-        Person person2 = new Person("Dmitry", new Coordinates(2, 2.0f),
-                3L, LocalDateTime.now(), "123456", Color.BROWN, null);
-//        System.out.println(person.getId());
-//        String path = System.getenv("Lab5");
-//        System.out.println(path);
-//        System.out.println(person.toString());
-//        YamlWriter yamlWriter = new YamlWriter();
-//        yamlWriter.writeYaml(person, "src/main/resources/Person.yaml");
-//        yamlWriter.writeYaml(person2, "src/main/resources/Person.yaml");
+    public static void main(String[] args) {
+        Class<Person[]> type = Person[].class;
+        String path = "Person.yaml";
         YamlReader yamlReader = new YamlReader();
-        System.out.println(yamlReader.readYaml("Person.yaml", Person.class));
+        try {
+            System.out.println(Arrays.toString(yamlReader.
+                    readYaml(path, type)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
