d736714f4437a24669a79c89ea53584167c0aa00
Dmitrii-Chebanenko <dchebanenko95@gmail.com>
2023-02-17 00:17:19 +0300

    add info

--- src/main/java/commands/CommandManager.java
+++ src/main/java/commands/CommandManager.java
@@ -13,18 +13,23 @@ public class CommandManager {
     public CommandManager(){
         map = new HashMap<>();
         AddToCollection addToCollection = new AddToCollection();
+        Info info = new Info();
         CommandExecuteInterface add = addToCollection::execute;
+        CommandExecuteInterface informationAboutCollection = info::execute;
         map.put("add", add);
+        map.put("info", informationAboutCollection);
 
     }
 
     public void startWorking(){
         System.out.println("CommandManager stated");
         Scanner scanner = new Scanner(System.in);
+        System.out.print("$ ");
         String str = scanner.nextLine();
         while(str!="exit") {
             var a = str.split(" ");
             map.get(a[0]).execute(GlobalObj.dataBase);
+            System.out.print("$ ");
             str = scanner.nextLine();
         }
     }
