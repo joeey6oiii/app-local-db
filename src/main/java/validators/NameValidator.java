73a69a5602e7944234b6cf60acf32fde274fc5f2
Alexey <thelegendofflaity@gmail.com>
2023-02-16 19:26:05 +0300

    hot fix for Loader. Validators no more trying to change the given value

--- src/main/java/validators/NameValidator.java
+++ src/main/java/validators/NameValidator.java
@@ -1,15 +1,9 @@
 package validators;
 
-import java.util.Scanner;
-
 public class NameValidator implements ValidateAble<String>{
-    Scanner scanner = new Scanner(System.in);
 
     @Override
     public boolean validate(String name) {
-        while (name == null || name.equals("")){
-            name = scanner.nextLine();
-        }
-        return true;
+        return !(name == null || name.equals(""));
     }
 }
