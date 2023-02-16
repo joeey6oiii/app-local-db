88aec21600db6c8160ffa117234383d791415ce3
Dmitrii-Chebanenko <dchebanenko95@gmail.com>
2023-02-17 00:17:19 +0300

    add info

---
+++ src/main/java/commands/Info.java
@@ -0,0 +1,14 @@
+package commands;
+
+import dataBase.DataBase;
+
+public class Info extends BaseCommand{
+    public void execute(DataBase obj){
+        System.out.println("Type: " + obj.getTypeOfTheCollection());
+        System.out.println("Length: " + obj.getSizeOfTheCollection());
+        System.out.println("Initialization Time: " + obj.getInitializationTime());
+    }
+    public void describe(){
+        System.out.println("print information about the collection to the standard output stream (type, initialization date, number of elements, etc.)");
+    }
+}
