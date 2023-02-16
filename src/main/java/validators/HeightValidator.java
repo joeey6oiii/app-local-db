1a349cfb08030b2f2c396ab3fcab23de7ee2cc2a
Alexey <thelegendofflaity@gmail.com>
2023-02-16 19:26:05 +0300

    hot fix for Loader. Validators no more trying to change the given value

--- src/main/java/validators/HeightValidator.java
+++ src/main/java/validators/HeightValidator.java
@@ -1,15 +1,9 @@
 package validators;
 
-import java.util.Scanner;
-
 public class HeightValidator implements ValidateAble<Long>{
-    Scanner scanner = new Scanner(System.in);
 
     @Override
     public boolean validate(Long height) {
-        while (height == null || height <= 0){
-            height = scanner.nextLong();
-        }
-        return true;
+        return !(height == null || height <= 0);
     }
 }
--- src/main/java/validators/PassportIDValidator.java
+++ src/main/java/validators/PassportIDValidator.java
@@ -1,17 +1,14 @@
 package validators;
 
 import java.util.Objects;
-import java.util.Scanner;
 
 public class PassportIDValidator implements ValidateAble<String>{
-    Scanner scanner = new Scanner(System.in);
 
     @Override
     public boolean validate(String passportID) {
-        while (Objects.equals(passportID, "") ||
-                passportID.length() < 4 || passportID.length() > 32){
-            passportID = scanner.nextLine();
+        if (passportID == null){
+            return true;
         }
-        return true;
+        return !(Objects.equals(passportID, "") || (passportID.length() < 4) || (passportID.length() > 32));
     }
 }
