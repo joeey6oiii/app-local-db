fae6a0d16891ac6cdff6dae3261e145c2d240d1b
Dmitrii-Chebanenko <dchebanenko95@gmail.com>
2023-02-17 10:49:13 +0300

    fix in command manager

--- src/main/java/commands/Save.java
+++ src/main/java/commands/Save.java
@@ -6,11 +6,11 @@ import yamlsTools.YamlWriter;
 import java.io.IOException;
 
 public class Save extends BaseCommand {
-    public void execute(DataBase obj){
+    public void execute(DataBase obj) {
         YamlWriter yamlWriter = new YamlWriter();
         try {
             yamlWriter.writeYaml(obj.getCollection());
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Something went wrong, the file was not created");
         }
         System.out.println("Your collection has been saved. Your file is on the path \"src/main/resources/orderOutput.yaml\"");
