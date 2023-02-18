795f1b5073d18178ed0329b51c72ddf00374da4f
Alexey <thelegendofflaity@gmail.com>
2023-02-18 11:52:32 +0300

    added BirthdayValidator and HairColorValidator implementation (still under question). PersonValidator uses none of these (not necessary since they are more suitable with reader and creator)

--- src/main/java/validators/HairColorValidator.java
+++ src/main/java/validators/HairColorValidator.java
@@ -3,9 +3,12 @@ package validators;
 import defaultClasses.Color;
 
 public class HairColorValidator implements ValidateAble<Color> {
+
     @Override
     public boolean validate(Color color) {
-        // TODO
-        return true;
+        if (color == null) {
+            return true;
+        }
+        return Color.listValues().contains(color);
     }
 }
