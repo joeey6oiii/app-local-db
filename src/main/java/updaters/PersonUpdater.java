696fa8e698930b32b47a01d828116edfc1cf1318
Alexey <thelegendofflaity@gmail.com>
2023-02-20 18:16:03 +0300

    improved "update" command

--- src/main/java/updaters/PersonUpdater.java
+++ src/main/java/updaters/PersonUpdater.java
@@ -2,14 +2,12 @@ package updaters;
 
 import defaultClasses.*;
 import generators.*;
+import helpFun.Reflection;
 import helpFun.StringToLocalDatetimeParser;
 import validators.PersonValidator;
 
-import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class PersonUpdater implements UpdateAble<Person> {
     private static final String ANSI_YELLOW;
@@ -18,6 +16,7 @@ public class PersonUpdater implements UpdateAble<Person> {
     private static final Scanner scanner;
     private static final PersonValidator personValidator;
     private static final Map<String, Generate> map;
+    private static final List<String> list;
 
     static {
         ANSI_YELLOW = "\u001B[33m";
@@ -28,10 +27,16 @@ public class PersonUpdater implements UpdateAble<Person> {
 
         personValidator = new PersonValidator();
 
-        map = new LinkedHashMap<>();
+        map = new LinkedHashMap<>(3);
         map.put("hairColor", new ColorGenerator());
         map.put("location", new LocationGenerator());
         map.put("coordinates", new CoordinatesGenerator());
+
+        list = new ArrayList<>(3);
+        list.add("name");
+        list.add("passportID");
+        list.add("birthday");
+        list.add("height");
     }
 
     @Override
@@ -43,114 +48,78 @@ public class PersonUpdater implements UpdateAble<Person> {
         String field = "";
         while (!field.equalsIgnoreCase("EXIT")) {
             field = scanner.nextLine();
-            if (map.containsKey(field)) {
+            if (field.equalsIgnoreCase("EXIT")) {
+                break;
+            } else if (map.containsKey(field)) {
                 Class<?> fieldClass = null;
                 var parameter = map.get(field).generate();
                 try {
                     fieldClass = person.getClass().getDeclaredField(field).getType();
                 } catch (NoSuchFieldException e) {
-                    System.out.print("No such field \n$ ");
+                    System.out.println(ANSI_RED + e.getMessage() + "\n"
+                            + e.getCause() + ANSI_RESET); System.exit(0);
                 }
-                field = Character.toUpperCase(field.charAt(0)) + field.substring(1);
-                field = "set" + field;
-                try {
-                    person.getClass().getMethod(field, fieldClass).invoke(person, parameter);
-                } catch (SecurityException | NoSuchMethodException
-                         | InvocationTargetException | IllegalAccessException e) {
-                    System.out.print("Something went wrong, please, try again \n$ ");
-                }
+                var previous_value = Reflection.getValue(person, field);
+                Reflection.setValue(person, field, fieldClass, parameter);
                 if (personValidator.validate(person)) {
-                    System.out.println(field.substring(3) + " has been successfully" +
-                            " updated for Person{id=" + person.getId() + "}!");
+                    System.out.println(field + " has been successfully updated for Person{id=" + person.getId() + "}!");
                     type();
                 } else {
-                    System.out.print(ANSI_RED + "Validation failed" +
-                            ANSI_RESET + ", please, try again \n$ ");
+                    Reflection.setValue(person, field, fieldClass, previous_value);
+                    System.out.print(ANSI_RED + "Validation failed" + ANSI_RESET + ", please, try again \n$ ");
                 }
-            } else {
-                if (field.equalsIgnoreCase("EXIT")) {
-                    break;
-                }
-                else if (field.equalsIgnoreCase("NAME")) {
-                    var previous_value = person.getName();
-                    System.out.print("Input name: String (not null) \n$ ");
-                    person.setName(scanner.nextLine());
-                    if (personValidator.getNameValidator().validate(person.getName())) {
-                        System.out.println("Name has been successfully" +
-                                " updated for Person{id=" + person.getId() + "}!");
-                        type();
-                    } else {
-                        person.setName(previous_value);
-                        System.out.print(ANSI_RED + "Validation failed" +
-                                ANSI_RESET + ", please, try again \n$ ");
-                    }
-                }
-                else if (field.equalsIgnoreCase("PASSPORTID")) {
-                    var previous_value = person.getPassportID();
-                    System.out.print("Input passportId: String (nullable; range: 4 <= Input <= 32) \n$ ");
-                    person.setPassportID(scanner.nextLine());
-                    if (personValidator.getPassportIDValidator().validate(person.getPassportID())) {
-                        System.out.println("PassportId has been successfully updated for Person{id="
-                                + person.getId() + "}!");
-                        type();
-                    } else {
-                        person.setPassportID(previous_value);
-                        System.out.print(ANSI_RED + "Validation failed" +
-                                ANSI_RESET + ", please, try again \n$ ");
-                    }
-                }
-                else if (field.equalsIgnoreCase("HEIGHT")) {
-                    var previous_value = person.getHeight();
-                    System.out.print("Input height: Long (not null) \n$ ");
+            } else if (list.contains(field)) {
+                var previous_value = Reflection.getValue(person, field);
+                boolean boolBirthday = field.equalsIgnoreCase("BIRTHDAY");
+                boolean boolHeight = field.equalsIgnoreCase("HEIGHT");
+                if (boolBirthday) {
+                    System.out.print("Input " + field + " (not null). Format: yyyy-MM-dd HH:mm:ss \n$ ");
                     boolean checked = false;
-                    do {
-                        try {
-                            person.setHeight(Long.valueOf(scanner.nextLine()));
-                            if (personValidator.getHeightValidator().validate(person.getHeight())) {
-                                System.out.println("Height has been successfully updated for Person{id="
-                                        + person.getId() + "}!");
-                            } else {
-                                person.setHeight(previous_value);
-                                System.out.print(ANSI_RED + "Validation failed" +
-                                        ANSI_RESET + ", please, try again \n$ ");
-                            }
-                            type();
-                            checked = true;
-                        } catch (Exception e) {
-                            System.out.print("Input height: Long (not null) \n$ ");
-                        }
-                    } while (!checked);
-                } else if (field.equalsIgnoreCase("BIRTHDAY")) {
-                    var previous_value = person.getBirthday();
-                    boolean checked = false;
-                    String strBirthday;
-                    LocalDateTime localDateTimeBirthday = null;
-                    System.out.print("Input birthday: LocalDateTime (not null)." +
-                            " Format: yyyy-MM-dd HH:mm:ss \n$ ");
+                    String strBirthday; LocalDateTime birthday = null;
                     do {
                         try {
                             strBirthday = scanner.nextLine();
-                            localDateTimeBirthday = StringToLocalDatetimeParser.stringToLocalDateTime(strBirthday);
+                            birthday = StringToLocalDatetimeParser.stringToLocalDateTime(strBirthday);
                             checked = true;
                         } catch (Exception e) {
-                            System.out.print("Input birthday: LocalDateTime (not null)." +
-                                    " Format: yyyy-MM-dd HH:mm:ss \n$ ");
+                            System.out.print("Input " + field + " (not null). Format: yyyy-MM-dd HH:mm:ss \n$ ");
                         }
                     } while (!checked);
-                    person.setBirthday(localDateTimeBirthday);
-                    if (personValidator.getBirthdayValidator().validate(person.getBirthday())) {
-                        System.out.println("Birthday has been successfully updated for Person{id="
-                                + person.getId() + "}!");
-                        type();
+                    Reflection.setValue(person, field, LocalDateTime.class, birthday);
+                } else if (boolHeight){
+                    System.out.print("Input " + field + " (not null) \n$ ");
+                    boolean checked = false;
+                    Long height = null;
+                    do {
+                        try {
+                            height = Long.valueOf(scanner.nextLine());
+                            checked = true;
+                        } catch (Exception e) {
+                            System.out.print("Input " + field + " (not null) \n$ ");
+                        }
+                    } while (!checked);
+                    Reflection.setValue(person, field, Long.class, height);
+                } else {
+                    System.out.print("Input " + field + " (not null) \n$ ");
+                    Reflection.setValue(person, field, String.class, scanner.nextLine());
+                }
+                if (personValidator.validate(person)) {
+                    System.out.println(field + " has been successfully updated for Person{id=" + person.getId() + "}!");
+                    type();
+                } else {
+                    if (boolBirthday) {
+                        Reflection.setValue(person, field, LocalDateTime.class, previous_value);
+                    } else if (boolHeight) {
+                        Reflection.setValue(person, field, Long.class, previous_value);
                     } else {
-                        person.setBirthday(previous_value);
-                        System.out.print(ANSI_RED + "Validation failed" +
-                                ANSI_RESET + ", please, try again \n$ ");
+                        Reflection.setValue(person, field, String.class, previous_value);
                     }
+                    System.out.print(ANSI_RED + "Validation failed" +
+                            ANSI_RESET + ", please, try again \n$ ");
                 }
-                else {
-                    System.out.print("No such field or field is unavailable to update \n$ ");
-                }
+            }
+            else {
+                System.out.print("No such field or field is unavailable to update \n$ ");
             }
         }
         System.out.println(ANSI_YELLOW + "Exit Person Updater" + ANSI_RESET);
